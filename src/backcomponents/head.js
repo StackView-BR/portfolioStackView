753990481e97e499c5359d1f19e370a04f53fbb0
Gabriel Lisbão <gabriel.lisbao@gmail.com>
2021-03-07 15:24:31 -0300

    celular frame

--- src/backcomponents/head.js
+++ src/backcomponents/head.js
@@ -12,36 +12,37 @@ function Head({ title }) {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:title" content="StackView" key="title" />
       <meta property="og:description" content="Automação, Iot e Full-Stack, Desenvolvimento Sistemas de controle e manutenção, Aplicativos, PWA, websites responsivos e gestão de dados" key="description" />
-      <meta property="og:image" href="/preview.png" />
+      <meta property="og:image" content="/preview.png" />
+      <meta property="og:image:type" content="image/png" />
 
       <meta name="description" content="Automação, Iot e Full-Stack, Desenvolvimento Sistemas de controle e manutenção, Aplicativos, PWA, websites responsivos e gestão de dados" />
       <meta name="keywords" content="engenharia, programação, webdesing, dev, automação, internet das coisas, iot, full-stack, desenvolvimento, controle, manutenção, softwares, aplicativos, pwa, website, site, responsivo, banco de dados, dados, sistemas, sensores, embarcados, sistemas digitais, webdesign, design, marketing, digital" />
       <link rel="shortcut icon" href="/static/favicon.ico" />
       <link rel="image_src" href="/preview.png" />
 
-      <meta name="theme-color" content="#000000"/>
+      <meta name="theme-color" content="#000000" />
 
-        <meta name="msapplication-navbutton-color" content="#000000"/>
+      <meta name="msapplication-navbutton-color" content="#000000" />
 
-          <meta name="apple-mobile-web-app-status-bar-style" content="#000000"/>
-          
-          <meta property="og:locale" content="pt-br" />
-          <meta http-equiv="Content-Language" content="pt-br" />
-          <link
-            rel="preload"
-            href="/fonts/REZ.woff"
-            as="font"
-            type="font/woff"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/XXIX.woff"
-            as="font"
-            type="font/woff"
-            crossOrigin=""
-          />
-          <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'></link>
+      <meta name="apple-mobile-web-app-status-bar-style" content="#000000" />
+
+      <meta property="og:locale" content="pt-br" />
+      <meta http-equiv="Content-Language" content="pt-br" />
+      <link
+        rel="preload"
+        href="/fonts/REZ.woff"
+        as="font"
+        type="font/woff"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/XXIX.woff"
+        as="font"
+        type="font/woff"
+        crossOrigin=""
+      />
+      <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'></link>
     </NextHead >
   );
 }
