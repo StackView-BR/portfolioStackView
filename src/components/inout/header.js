4665f3f0997286cfcca9ab02bfce0b61920bb72e
Gabriel Elisbão <gabriel.lisbao@gmail.com>
2021-03-21 16:23:54 -0300

    scrol

--- src/components/inout/header.js
+++ src/components/inout/header.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 import * as Scroll from 'react-scroll';
 
 class Header extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
@@ -13,46 +13,48 @@ class Header extends React.Component {
       back: "rgba(0,0,0,0.1)",
       flip: "rgba(0,0,0,0.1)",
       open: false,
-      offset:"-100%",
+      offset: "-100%",
       to: "i",
       to2: "i",
       rotate: "-90",
       display: "none",
 
     };
-    
-    
+
+
     this.handleScroll = this.handleScroll.bind(this);
 
   }
- 
 
-  handleContact =() => {
-    console.log("clique");
-    const scroll    = Scroll.animateScroll;
-      scroll.scrollToBottom();
-   
+  handleUp = () => {
+    const scroll = Scroll.animateScroll;
+    scroll.scrollToTop();
+  }
+  handleContact = () => {
+    const scroll = Scroll.animateScroll;
+    scroll.scrollToBottom();
+
   }
 
-  handleClick = (e) =>  {
-    if(this.state.open){
+  handleClick = (e) => {
+    if (this.state.open) {
       this.setState({
         open: false,
         offset: "-100%",
         to: "i",
-      to2: "i",
-      rotate: "-90",
+        to2: "i",
+        rotate: "-90",
 
       })
-    }else{
+    } else {
       this.setState({
         open: true,
         offset: "0%",
         back: "black",
         flip: "white",
         to: "a",
-      to2: "",
-      rotate: "0",
+        to2: "",
+        rotate: "0",
 
       })
 
@@ -73,7 +75,7 @@ class Header extends React.Component {
         display: "",
         flip: "white",
       })
-    } else if ((window.pageYOffset >= totalHeight || window.pageYOffset <= window.innerHeight)&&!this.state.open) {
+    } else if ((window.pageYOffset >= totalHeight || window.pageYOffset <= window.innerHeight) && !this.state.open) {
       this.setState({
         back: "rgba(0,0,0,0.1)",
         display: "none",
@@ -81,13 +83,13 @@ class Header extends React.Component {
       })
     }
     this.setState({
-      progress: (window.pageYOffset / totalHeight) * 100 + "%",
+      progress: 100 - ((window.pageYOffset / totalHeight) * 100) + "%",
     });
 
   };
 
   render() {
-    
+
 
     return (
       <div>
@@ -102,46 +104,48 @@ class Header extends React.Component {
 
           </div>
 
-          <div onClick={this.handleClick} style={{ backgroundColor: this.state.flip, transform: "rotate("+this.state.rotate+"deg)" }} className="navicon apareca"><div className="center"><a className="view">{this.state.to2}</a><a className="stack">{this.state.to}</a></div></div>
-          <div className="collapso apareca" style={{ transform: "translateY(" + this.state.offset +")" }}>
+          <div onClick={this.handleClick} style={{ backgroundColor: this.state.flip, transform: "rotate(" + this.state.rotate + "deg)" }} className="navicon apareca"><div className="center"><a className="view">{this.state.to2}</a><a className="stack">{this.state.to}</a></div></div>
+          <div className="collapso apareca" style={{ transform: "translateY(" + this.state.offset + ")" }}>
             <div className="menu">
-            <img
-            id="icon"
-            src="/icons/icon.png"
-            height="70px"
-        />
-        <div className="links"><Link href="https://www.behance.net/StackView" ><Button><a>Portfolio</a></Button></Link> <br/>
-            <Link href="/we" ><Button><a>Developers</a></Button></Link><br/>
-            <Link href="" onClick={this.handleContact} ><Button><a>Contact Us</a></Button></Link></div><br/>
-            <div className="info">
-            <a>Phone</a>: <br /> +5517988309968<br /><br />
-            <a>Email</a>: <br />stackviewbr@gmail.com<br /><br />
+              <img
+                id="icon"
+                src="/icons/icon.png"
+                height="70px"
+              />
+              <div className="links"><Link href="https://www.behance.net/StackView" ><Button><a>Portfolio</a></Button></Link> <br />
+                <Link href="/we" ><Button><a>Developers</a></Button></Link><br />
+                <Link href="" onClick={this.handleContact} ><Button><a>Contact Us</a></Button></Link></div><br />
+              <div className="info">
+                <a>Phone</a>: <br /> +5517988309968<br /><br />
+                <a>Email</a>: <br />stackviewbr@gmail.com<br /><br />
                 <a>Address</a>:  <br />Av. João Naves de Ávila, 2121<br /> Santa Mônica, Uberlândia - MG, 38408-100, Brasil
                 <div className="w"><a ><Link href="https://api.whatsapp.com/send?phone=5517988309968"><img
-                id="w"
-                src="/icons/whats.png"
-                width="auto"
-                
+                  id="w"
+                  src="/icons/whats.png"
+                  width="auto"
 
-            /></Link></a>
-                <a className="t" ><Link href="https://t.me/stackviewbr"><img
+
+                /></Link></a>
+                  <a className="t" ><Link href="https://t.me/stackviewbr"><img
                     id="t"
                     src="/icons/tel.png"
                     width="auto"
 
-                /></Link></a></div>
-            </div>
-            
+                  /></Link></a></div>
               </div>
+
+            </div>
             <div className="stackview"><div className="rotate"><a className="stack">stack</a><a className="view">view</a></div></div>
           </div>
 
         </div>
 
-        <div className="fakebar"></div>
+        <div className="fakebar">
+        <div className="progress" style={{background: "linear-gradient(90deg, white 0,  white "+ this.state.progress +",black "+ this.state.progress +",black 100%)"}}></div>
         <div className="bar">
-          <div className="progress" style={{ height: this.state.progress }} ></div>
-        </div>
+          <Link href="" onClick={this.handleUp}><a >Scroll Up</a></Link>
+        </div></div>
+
       </div>
     );
   }
