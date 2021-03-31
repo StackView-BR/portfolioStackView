91b803c97dce1108e08dc3374e7fd2ef1f161e6f
Gabriel Elisbão <gabriel.lisbao@gmail.com>
2021-03-31 02:58:11 -0300

    pwa

---
+++ src/components/view/clients.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Front extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+          
+      }
+     
+
+render(){
+
+    return <div className="clientes" style={{}}>
+        
+
+    </div>
+}
+    
+
+
+}
+
+export default Front;
